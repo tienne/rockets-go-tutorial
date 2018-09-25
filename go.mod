d294863164827d29a9380fc2c7663f7482bc9c9d
tienne <tienne89@gmail.com>
2018-09-25 18:44:55 +0900

    step4

--- go.mod
+++ go.mod
@@ -1,1 +1,9 @@
 module github.com/tienne/rockets-go-tutorial
+
+require (
+	github.com/esimov/caire v1.1.0
+	github.com/esimov/pigo v1.0.3 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/pkg/errors v0.8.0
+	golang.org/x/image v0.0.0-20180920235150-e1a1ede6891c // indirect
+)
--- main.go
+++ main.go
@@ -3,51 +3,22 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/tienne/rockets-go-tutorial/seam"
 	"io"
 	"log"
 	"net/http"
-
-	"github.com/esimov/caire"
-	"github.com/pkg/errors"
 )
 
 const (
 	IMAGE_URL string = "https://bit.ly/2QGPDkr"
 )
 
-func ContentAwareResize(url string) ([]byte, error) {
-	fmt.Printf("Download starting for url %s\n", url)
-	response, err := http.Get(url)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to read the image")
-	}
-	defer response.Body.Close()
-
-	converted := &bytes.Buffer{}
-	fmt.Printf("Download complete %s", url)
-
-	shrinkFactor := 30
-	fmt.Printf("Resize in progress %s, shrinking width by %d percent...\n", url, shrinkFactor)
-	p := &caire.Processor{
-		NewWidth:   shrinkFactor,
-		Percentage: true,
-	}
-
-	err = p.Process(response.Body, converted)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to apply seam carving to the image")
-	}
-	fmt.Printf("Seam carving completed for %s\n", url)
-
-	return converted.Bytes(), nil
-}
-
 func main() {
 	fmt.Println("Ready for liftoff! Checkout http://localhost:3000/occupymars")
 
 	http.HandleFunc("/occupymars", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Query().Get("resize") > "" {
-			resized, err := ContentAwareResize(IMAGE_URL)
+			resized, err := seam.ContentAwareResize(IMAGE_URL)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			}
